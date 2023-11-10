de87412f517e5ca5157ad250818c6d5df80b4857
Murtaz <itsmurtaz@gmail.com>
2023-11-11 01:26:55 +0530

    Added check in SQL file not to create database if it already exists

--- database/create_db.sql
+++ database/create_db.sql
@@ -1,2 +1,3 @@
-CREATE DATABASE courseservice;
+SELECT 'CREATE DATABASE courseservice'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'courseservice')\gexec;
 GRANT ALL PRIVILEGES ON DATABASE courseservice TO postgres;

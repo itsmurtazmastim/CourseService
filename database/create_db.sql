94166a32581a415cf5d2fb8d0866cddc62195296
Murtaz <itsmurtaz@gmail.com>
2023-11-13 19:49:30 +0530

    Updating to resolve merge conflicts

--- database/create_db.sql
+++ database/create_db.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 DO $$
 BEGIN
  IF NOT EXISTS(SELECT FROM pg_database WHERE datname = 'courseservice') THEN
@@ -6,15 +5,3 @@ BEGIN
     GRANT ALL PRIVILEGES ON DATABASE courseservice TO postgres;
  END IF;
 END $$;
-=======
---SELECT 'CREATE DATABASE courseservice'
---WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'courseservice')\gexec;
---GRANT ALL PRIVILEGES ON DATABASE courseservice TO postgres;
-DO $$
-BEGIN
-  IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'courseservice') THEN
-    CREATE DATABASE courseservice;
-    GRANT ALL PRIVILEGES ON DATABASE courseservice TO postgres;
-  END IF;
-END $$;
->>>>>>> a370d7b534c76dce922b3bdcde3d9755981b1b9d
